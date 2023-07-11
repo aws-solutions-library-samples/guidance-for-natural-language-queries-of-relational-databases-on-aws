21eb92989c8365fd1cdfd87f023e4a38e99b6732
Gary A. Stafford (AWS) <138904992+garystafford-aws@users.noreply.github.com>
2023-07-10 22:17:43 -0400

    Initial commit of project files (#1)
    * Initial project commit
    
    * Update README
    
    * Update README
    
    * Update README
    
    * Update README
    
    * Update README

--- README.md
+++ README.md
@@ -1,11 +1,202 @@
-## My Project
+## Generative AI-enabled Natural Language Relational Database Queries
 
-TODO: Fill this README out!
+Natural Language Query (NLQ) demonstration using Amazon RDS for PostgreSQL and Amazon SageMaker JumpStart Foundation Models and optionally OpenAI's models via their API.
 
-Be sure to:
+## Foundation Model Choice and Accuracy of NQL
 
-* Change the title in this README
-* Edit your repository description on GitHub
+The selection of the Foundation Model (FM) for NLQ plays a crucial role in the application's ability to accurately translate natural language questions into natural language answers; not all FMs possess this capability. Additionally, the accuracy of the NLQ process relies heavily on the chosen model, as well as other factors such as the prompt, prompt-template, and sample queries used for in-context learning (also known as few-shot prompting).
+
+OpenAI GPT-3 and GPT-4 series models, including `text-davinci-003` (Legacy), `gpt-3.5-turbo`, and the latest addition, `gpt-4`. These models are considered state-of-the-art for NLQ, providing highly accurate responses to a wide range of complex NLQ questions, with minimal in-context learning. As an alternative to OpenAI, models such as the `google/flan-t5-xxl` and `google/flan-t5-xxl-fp16` models available through Amazon SageMaker JumpStart Foundation Models. It's important to note that while the `google/flan-t5` series of models are a popular choice, their capabilities for NLQ are only a fraction of what OpenAI's GPT-3 and GPT-4 series models offer. The `google/flan-t5-xxl-fp16` model may fail to return an answer, provide incorrect answers, or cause the JumpStart Foundation Model to experience timeouts when faced with even moderately complex questions.
+
+Transitioning from Amazon SageMaker JumpStart Foundation Models to OpenAI's models via their API eliminates the need for the deployment of the `NlqSageMakerEndpointStack` CloudFormation stack. If the stack has already been deployed, it can be deleted. The next build the Amazon ECR Docker Image using the `Dockerfile_OpenAI` Dockerfile and push it to the ECR repository. Finally, deploy the `NlqEcsOpenAIStack.yaml` CloudFormation template file. To utilize OpenAI's models, you will also need to create an OpenAI account and obtain your personal secret API key.
+
+You can also replace the `google/flan-t5-xxl-fp16` JumpStart Foundation Model hosted by the Amazon SageMaker endpoint, deployed by the existing `NlqSageMakerEndpointStack.yaml` CloudFormation template file. You will first need to modify the `NlqSageMakerEndpointStack.yaml` CloudFormation template file and update the deployed CloudFormation stack, `NlqSageMakerEndpointStack`. Additionally, you will have to make adjustments to the `app_sagemaker.py` file by modifying the `ContentHandler` Class to match the response payload of the chosen model. Rebuilding the Amazon ECR Docker Image using the `Dockerfile_SageMaker` Dockerfile and pushing it to the ECR repository will also be necessary. Lastly, you should update the deployed ECS task and service, which are part of the `NlqEcsStackSageMaker.yaml` CloudFormation template file (`NlqEcsStackSageMaker` CloudFormation stack).
+
+## Instructions
+
+1. If using the Amazon SageMaker JumpStart Foundation Models option, make you have the required EC2 instance for the endpoint inference, or request it using Service Quotas in the AWS Management Console (e.g., `ml.g5.24xlarge` for the `google/flan-t5-xxl-fp16` model: https://us-east-1.console.aws.amazon.com/servicequotas/home/services/sagemaker/quotas/L-6821867B).
+2. Create the required secrets in AWS Secret Manager using the AWS CLI.
+3. Deploy the `NlqMainStack` CloudFormation template.
+4. Build and push the `nlq-genai:1.0.0-sm` Docker image to the new Amazon ECR repository, or alternately the the `nlq-genai:1.0.0-oai` Docker image for use with Option 2: OpenAI API.
+5. Import the included sample data into the Amazon RDS MoMA database.
+6. Add the `nlqapp` user to the MoMA database.
+7. Deploy the `NlqSageMakerEndpointStack` CloudFormation template, using the Amazon SageMaker JumpStart Foundation Models option.
+8. Deploy the `NlqEcsSageMakerStack` CloudFormation template, or alternately the `NlqOpenAIStack` CloudFormation template for use with Option 2: OpenAI API.
+
+## 2. Create AWS Secret Manager Secrets
+
+Make sure you update usernames and password.
+
+```sh
+aws secretsmanager create-secret \
+    --name /nlq/MasterUsername \
+    --description "Master username for RDS instance." \
+    --secret-string <your_master_username>
+
+aws secretsmanager create-secret \
+    --name /nlq/NLQAppUsername \
+    --description "Master username for RDS instance." \
+    --secret-string <your_nqlapp_username>
+
+aws secretsmanager create-secret \
+    --name /nlq/NLQAppUserPassword \
+    --description "Master username for RDS instance." \
+    --secret-string "<your_nqlapp_password>"
+
+# optional for Option 2: OpenAI API/model
+aws secretsmanager create-secret \
+    --name /nlq/OpenAIAPIKey \
+    --description "OpenAI API key." \
+    --secret-string "<your_openai_api_key"
+```
+
+## 3. Deploy the Main NLQ Stack: Networking, Security, RDS Instance, and ECR Repository
+
+```sh
+cd cloudformation/
+
+aws cloudformation create-stack \
+  --stack-name NlqMainStack \
+  --template-body file://NlqMainStack.yaml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters ParameterKey="MyIpAddress",ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32
+```
+
+## 4. Build and Push the Docker Image to ECR
+
+You can build the image locally, in a CI/CD pipeline, using SageMaker Notebook environment, or AWS Cloud9.
+
+```sh
+# Located in the output from the nlq-genai-infra CloudFormation template
+cd docker/
+
+# e.g. 111222333444.dkr.ecr.us-east-1.amazonaws.com/nlq-genai
+ECS_REPOSITORY="<you_ecs_repository>"
+```
+
+Option 1: SageMaker JumpStart FM Endpoint
+
+```sh
+TAG="1.0.0-sm"
+
+aws ecr get-login-password --region us-east-1 | \
+	docker login --username AWS --password-stdin $ECS_REPOSITORY
+
+docker build -f Dockerfile_SageMaker -t $ECS_REPOSITORY:$TAG .
+
+docker push $ECS_REPOSITORY:$TAG
+```
+
+Option 2: OpenAI API
+
+```sh
+TAG="1.0.0-oai"
+
+docker build -f Dockerfile_OpenAI -t $ECS_REPOSITORY:$TAG .
+
+docker push $ECS_REPOSITORY:$TAG
+```
+
+## 5. Import Sample Data and Configure the MoMA Database
+
+1. Connect to the `moma` database using you preferred PostgreSQL tool. You may need to enable `Public access` for the RDS instance temporarily depending on how you connect to the database.
+2. Create the two MoMA collection tables into the `moma` database.
+
+```sql
+CREATE TABLE public.artists (
+	artist_id integer NOT NULL,
+	full_name character varying(200),
+	nationality character varying(50),
+	gender character varying(25),
+	birth_year integer,
+	death_year integer,
+	CONSTRAINT artists_pk PRIMARY KEY (artist_id)
+)
+
+CREATE TABLE public.artworks (
+	artwork_id integer NOT NULL,
+	title character varying(500),
+	artist_id integer NOT NULL,
+	date integer,
+	medium character varying(250),
+	dimensions text,
+	acquisition_date text,
+	credit text,
+	catalogue character varying(250),
+	department character varying(250),
+	classification character varying(250),
+	object_number text,
+	diameter_cm text,
+	circumference_cm text,
+	height_cm text,
+	length_cm text,
+	width_cm text,
+	depth_cm text,
+	weight_kg text,
+	durations integer,
+	CONSTRAINT artworks_pk PRIMARY KEY (artwork_id)
+)
+```
+
+3. Unzip and import the two data files into the `moma` database using the text files in the `/data` sub-directory. The both files contain a header row and pipe-delimited ('|').
+
+```txt
+# examples commands from pgAdmin4
+--command " "\\copy public.artists (artist_id, full_name, nationality, gender, birth_year, death_year) FROM 'moma_public_artists.txt' DELIMITER '|' CSV HEADER QUOTE '\"' ESCAPE '''';""
+
+--command " "\\copy public.artworks (artwork_id, title, artist_id, date, medium, dimensions, acquisition_date, credit, catalogue, department, classification, object_number, diameter_cm, circumference_cm, height_cm, length_cm, width_cm, depth_cm, weight_kg, durations) FROM 'moma_public_artworks.txt' DELIMITER '|' CSV HEADER QUOTE '\"' ESCAPE '''';""
+```
+
+## 6. Add NLP Application to the MoMA Database
+
+Create the read-only NQL Application database user account. Update the username and password you configured in step 2, with the secrets.
+
+```sql
+CREATE ROLE <your_nqlapp_username> WITH
+	LOGIN
+	NOSUPERUSER
+	NOCREATEDB
+	NOCREATEROLE
+	INHERIT
+	NOREPLICATION
+	CONNECTION LIMIT -1
+	PASSWORD '<your_nqlapp_password>';
+
+GRANT pg_read_all_data TO <your_nqlapp_username>;
+```
+
+## 7. Deploy the ML Stack: Model, Endpoint
+
+Option 1: SageMaker JumpStart FM Endpoint
+
+```sh
+cd cloudformation/
+
+aws cloudformation create-stack \
+  --stack-name NlqSageMakerEndpointStack \
+  --template-body file://NlqSageMakerEndpointStack.yaml \
+  --capabilities CAPABILITY_NAMED_IAM
+```
+
+## 8. Deploy the ECS Service Stack: Task, Service
+
+Option 1: SageMaker JumpStart FM Endpoint
+
+```sh
+aws cloudformation create-stack \
+  --stack-name NlqEcsSageMakerStack \
+  --template-body file://NlqEcsSageMakerStack.yaml \
+  --capabilities CAPABILITY_NAMED_IAM
+```
+
+Option 2: OpenAI API
+
+```sh
+aws cloudformation create-stack \
+  --stack-name NlqEcsOpenAIStack \
+  --template-body file://NlqEcsOpenAIStack.yaml \
+  --capabilities CAPABILITY_NAMED_IAM
+```
 
 ## Security
 
@@ -15,3 +206,4 @@ See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more inform
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
 
+````
---
+++ docker/app_openai.py
@@ -0,0 +1,315 @@
+# Natural Language Query (NLQ) demo using Amazon RDS for PostgreSQL and OpenAI GPT-3 series model.
+# Author: Gary A. Stafford (garystaf@amazon.com)
+# Date: 2023-07-10
+# Application expects the following environment variables (adjust for your environment):
+# export OPENAI_API_KEY=<your_api_key>
+# export REGION_NAME="us-east-1"
+# export MODEL_NAME="gpt-3.5-turbo"
+# Usage: streamlit run app.py --server.runOnSave true
+
+import json
+import logging
+import os
+
+import boto3
+import streamlit as st
+import yaml
+from botocore.exceptions import ClientError
+from langchain import (FewShotPromptTemplate, PromptTemplate, SQLDatabase,
+                       SQLDatabaseChain)
+from langchain.chains.sql_database.prompt import (PROMPT_SUFFIX,
+                                                  _postgres_prompt)
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.prompts.example_selector.semantic_similarity import \
+    SemanticSimilarityExampleSelector
+from langchain.vectorstores import Chroma
+from streamlit_chat import message
+
+REGION_NAME = os.environ.get("REGION_NAME", "us-east-1")
+MODEL_NAME = os.environ.get("MODEL_NAME", "gpt-3.5-turbo")
+
+
+def main():
+    st.set_page_config(
+        page_title="Natural Language Query (NLQ) Demo",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+    )
+
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    NO_ANSWER_MSG = "I'm sorry, I was not able to answer your question."
+
+    os.environ["OPENAI_API_KEY"] = set_openai_api_key(REGION_NAME)
+
+    llm = ChatOpenAI(
+        model_name=MODEL_NAME,
+        temperature=0.0,
+        verbose=True,
+    )
+
+    # define datasource uri
+    rds_uri = get_rds_uri(REGION_NAME)
+    db = SQLDatabase.from_uri(rds_uri)
+
+    # load examples for few-shot prompting
+    examples = load_samples()
+
+    sql_db_chain = load_few_shot_chain(llm, db, examples)
+
+    # Store the initial value of widgets in session state
+    if "visibility" not in st.session_state:
+        st.session_state.visibility = "visible"
+        st.session_state.disabled = False
+
+    if "generated" not in st.session_state:
+        st.session_state["generated"] = []
+
+    if "past" not in st.session_state:
+        st.session_state["past"] = []
+
+    if "query" not in st.session_state:
+        st.session_state["query"] = []
+
+    if "query_text" not in st.session_state:
+        st.session_state["query_text"] = []
+
+    # define streamlit colums
+    col1, col2 = st.columns([2, 1], gap="large")
+
+    # build the streamlit sidebar
+    build_sidebar()
+
+    # build the main app ui
+    build_form(col1, col2)
+
+    # get the users query
+    get_text(col1)
+    user_input = st.session_state["query"]
+
+    if user_input:
+        with st.spinner(text="In progress..."):
+            st.session_state.past.append(user_input)
+            try:
+                output = sql_db_chain(user_input)
+                st.session_state.generated.append(output)
+                logging.info(st.session_state["query"])
+                logging.info(st.session_state["generated"])
+            except Exception as exc:
+                st.session_state.generated.append(NO_ANSWER_MSG)
+                logging.error(exc)
+
+    if st.session_state["generated"]:
+        with col1:
+            for i in range(len(st.session_state["generated"]) - 1, -1, -1):
+                if (i >= 0) and (st.session_state["generated"][i] != NO_ANSWER_MSG):
+                    message(
+                        st.session_state["generated"][i]["result"],
+                        key=str(i),
+                        is_user=False,
+                        avatar_style="icons",
+                        seed="459",
+                    )
+                    message(
+                        st.session_state["past"][i],
+                        is_user=True,
+                        key=str(i) + "_user",
+                        avatar_style="icons",
+                        seed="158",
+                    )
+                else:
+                    message(
+                        NO_ANSWER_MSG,
+                        key=str(i),
+                        is_user=False,
+                        avatar_style="icons",
+                        seed="459",
+                    )
+                    message(
+                        st.session_state["past"][i],
+                        is_user=True,
+                        key=str(i) + "_user",
+                        avatar_style="icons",
+                        seed="158",
+                    )
+
+    position = len(st.session_state["generated"]) - 1
+    with st.sidebar:
+        if (position >= 0) and (
+            st.session_state["generated"][position] != NO_ANSWER_MSG
+        ):
+            st.write("Question:")
+            st.code(st.session_state["generated"][position]["query"], language="text")
+            st.write("SQL Query:")
+            st.code(
+                st.session_state["generated"][position]["intermediate_steps"][1],
+                language="sql",
+            )
+            st.write("Results:")
+            st.code(
+                st.session_state["generated"][position]["intermediate_steps"][3],
+                language="python",
+            )
+            st.write("Answer:")
+            st.code(st.session_state["generated"][position]["result"], language="text")
+        else:
+            st.write("Nothing to see here...")
+
+
+def set_openai_api_key(region_name):
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    openai_api_key = None
+
+    try:
+        secret = client.get_secret_value(SecretId="/nlq/OpenAIAPIKey")
+        openai_api_key = secret["SecretString"]
+    except ClientError as e:
+        logging.error(e)
+        raise e
+
+    return openai_api_key
+
+
+def get_rds_uri(region_name):
+    # SQLAlchemy 2.0 reference: https://docs.sqlalchemy.org/en/20/dialects/postgresql.html
+    # URI format: postgresql+psycopg2://user:pwd@hostname:port/dbname
+
+    rds_username = None
+    rds_password = None
+    rds_endpoint = None
+    rds_port = None
+    rds_db_name = None
+
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    try:
+        secret = client.get_secret_value(SecretId="/nlq/RDS_URI")
+        secret = json.loads(secret["SecretString"])
+        rds_endpoint = secret["RDSDBInstanceEndpointAddress"]
+        rds_port = secret["RDSDBInstanceEndpointPort"]
+        rds_db_name = secret["NLQAppDatabaseName"]
+
+        secret = client.get_secret_value(SecretId="/nlq/NLQAppUsername")
+        rds_username = secret["SecretString"]
+
+        secret = client.get_secret_value(SecretId="/nlq/NLQAppUserPassword")
+        rds_password = secret["SecretString"]
+    except ClientError as e:
+        logging.error(e)
+        raise e
+
+    return f"postgresql+psycopg2://{rds_username}:{rds_password}@{rds_endpoint}:{rds_port}/{rds_db_name}"
+
+
+def load_samples():
+    # Use the corrected examples for few-shot prompting examples
+    sql_samples = None
+
+    with open("sql_examples_postgresql.yaml", "r") as stream:
+        sql_samples = yaml.safe_load(stream)
+
+    return sql_samples
+
+
+def load_few_shot_chain(llm, db, examples):
+    example_prompt = PromptTemplate(
+        input_variables=["table_info", "input", "sql_cmd", "sql_result", "answer"],
+        template=(
+            "{table_info}\n\nQuestion: {input}\nSQLQuery: {sql_cmd}\nSQLResult:"
+            " {sql_result}\nAnswer: {answer}"
+        ),
+    )
+
+    local_embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    example_selector = SemanticSimilarityExampleSelector.from_examples(
+        examples,
+        local_embeddings,
+        Chroma,
+        k=min(3, len(examples)),
+    )
+
+    few_shot_prompt = FewShotPromptTemplate(
+        example_selector=example_selector,
+        example_prompt=example_prompt,
+        prefix=_postgres_prompt + "Here are some examples:",
+        suffix=PROMPT_SUFFIX,
+        input_variables=["table_info", "input", "top_k"],
+    )
+
+    return SQLDatabaseChain.from_llm(
+        llm,
+        db,
+        prompt=few_shot_prompt,
+        use_query_checker=False,
+        verbose=True,
+        return_intermediate_steps=True,
+    )
+
+
+def get_text(col1):
+    with col1:
+        input_text = st.text_input(
+            "Ask a question:",
+            "",
+            key="query_text",
+            placeholder="Your question here...",
+            on_change=clear_text(),
+        )
+        logging.info(input_text)
+
+
+def clear_text():
+    st.session_state["query"] = st.session_state["query_text"]
+    st.session_state["query_text"] = ""
+
+
+def build_sidebar():
+    with st.sidebar:
+        with st.container():
+            st.header("Under the Hood")
+
+
+def build_form(col1, col2):
+    with col1:
+        with st.container():
+            st.markdown("# Natural Language Query (NLQ) Demo")
+            st.markdown("## Ask questions of your data using natural language.")
+
+        with st.container():
+            with st.expander("Click here for sample questions..."):
+                st.text(
+                    """
+                How many artists are there in the collection?
+                How many pieces of artwork are there in the collection?
+                How many artists are there whose nationality is Italian?
+                How many artworks are by the artist Claude Monet?
+                How many artworks are classified as paintings?
+                How many artworks were created by Spanish artists?
+                How many artist names start with the letter 'M'?
+                Who is the most prolific artist in the collection? What is their nationality?
+                What nationality of artists created the most artworks in the collection?
+                What is the ratio of male to female artists. Return as a ratio.
+                What are the five oldest artworks in the collection? Return the title and date for each.
+                Return the artwork for Frida Kahlo in a numbered list, including title and date.
+                Give me a recipe for chocolate cake.
+                """
+                )
+    with col2:
+        with st.container():
+            st.button("clear chat", on_click=clear_session)
+
+
+def clear_session():
+    for key in st.session_state.keys():
+        del st.session_state[key]
+
+
+if __name__ == "__main__":
+    main()
